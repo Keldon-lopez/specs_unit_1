4fd7ba83d4eb8a20eaffc38791384bafe792aeae
Keldon Lopez <keldon.lopz@gmail.com>
2023-05-16 14:22:19 -0400

    completed step 4

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -5,11 +5,17 @@ import { useState } from 'react';
 
 
 function App() {
-  const [square, setSquare] = useState(['','','','','','','','','']);
+  const [squares, setSquares] = useState(['','','','','','','','','']);
   const [player, setPlayer] = useState(true);
+
   return (
     <div className="App">
-      <Square square={square} setSquare={setSquare} player={player} setPlayer={setPlayer}></Square>
+      <div className='container'>
+        {squares.map((value,index) => {
+          return <Square squares={squares} setSquares={setSquares} player={player} setPlayer={setPlayer} squareValue={value} index={index}></Square>
+        })}
+      {/* <Square square={square} setSquare={setSquare} player={player} setPlayer={setPlayer}></Square> */}
+      </div>
     </div>
   );
 }
--- tic-tac-toe/src/Square.jsx
+++ tic-tac-toe/src/Square.jsx
@@ -1,10 +1,25 @@
 import  React  from 'react';
 
 const Square = (props) => {
-    console.log(props);
+    const { index, player, setPlayer, setSquares, squares, squareValue} = props;
+    const handleClick = () => {
+        console.log("I can handle my own click")
+        if (!squareValue) {
+            if (player) {
+                squares.splice(index, 1, "X");
+                setSquares(squares);
+                setPlayer(!player);
+            } else {
+                squares.splice(index, 1, "O");
+                setSquares(squares);
+                setPlayer(!player);
+            }
+        }
+    };
+    
     return (
-        <div className='square'>
-            {props.propVar}
+        <div className='square' onClick={handleClick}>
+            {squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : squareValue}
         </div>
     )
 } 
