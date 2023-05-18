f00f7d76d0f141ec8a227adc4e458687f6ca9cb2
Keldon Lopez <keldon.lopz@gmail.com>
2023-05-18 13:55:11 -0400

    calculated winner

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,29 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-import Square from './Square.jsx';
-import { useState } from 'react';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Square from "./Square.jsx";
+import { useState } from "react";
 
 function App() {
-  const [squares, setSquares] = useState(['','','','','','','','','']);
+  const [squares, setSquares] = useState(["", "", "", "", "", "", "", "", ""]);
   const [player, setPlayer] = useState(true);
+  const [winner, setWinner] = useState("");
 
-  const handleClick = () =>{
-    setSquares(['','','','','','','','','']);
+  const handleClick = () => {
+    setSquares(["", "", "", "", "", "", "", "", ""]);
     setPlayer(true);
-    console.log(squares);
-    console.log(player)
-  }
+    setWinner("");
+  };
 
   return (
     <div className="App">
       <button onClick={handleClick}>Reset</button>
-      <div className='container'>
-        {squares.map((value,index) => {
-          return <Square squares={squares} setSquares={setSquares} player={player} setPlayer={setPlayer} squareValue={value} index={index}></Square>
+      <div className="container">
+        {squares.map((value, index) => {
+          return (
+            <Square
+              setWinner={setWinner}
+              squares={squares}
+              setSquares={setSquares}
+              player={player}
+              setPlayer={setPlayer}
+              squareValue={value}
+              index={index}
+            ></Square>
+          );
         })}
-      {/* <Square square={square} setSquare={setSquare} player={player} setPlayer={setPlayer}></Square> */}
+        {/* <Square square={square} setSquare={setSquare} player={player} setPlayer={setPlayer}></Square> */}
       </div>
+      {winner && <span>Winner: {winner}</span>}
     </div>
   );
 }
--- tic-tac-toe/src/Square.jsx
+++ tic-tac-toe/src/Square.jsx
@@ -1,27 +1,61 @@
-import  React  from 'react';
+import React from "react";
 
 const Square = (props) => {
-    const { index, player, setPlayer, setSquares, squares, squareValue} = props;
-    const handleClick = () => {
-        console.log("I can handle my own click")
-        if (!squareValue) {
-            if (player) {
-                squares.splice(index, 1, "X");
-                setSquares(squares);
-                setPlayer(!player);
-            } else {
-                squares.splice(index, 1, "O");
-                setSquares(squares);
-                setPlayer(!player);
-            }
-        }
-    };
-    
-    return (
-        <div className='square' onClick={handleClick}>
-            {squareValue === "O" ? <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" /> : squareValue}
-        </div>
-    )
-} 
+  const {
+    index,
+    player,
+    setPlayer,
+    setSquares,
+    setWinner,
+    squares,
+    squareValue,
+  } = props;
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        setWinner(squares[a]);
+      }
+    }
+  };
+
+  const handleClick = () => {
+    if (!squareValue) {
+      if (player) {
+        squares.splice(index, 1, "X");
+      } else {
+        squares.splice(index, 1, "O");
+      }
+      setSquares(squares);
+      setPlayer(!player);
+      calculateWinner(squares);
+    }
+  };
+
+  return (
+    <div className="square" onClick={handleClick}>
+      {squareValue === "O" ? (
+        <img src="https://cdn.discordapp.com/attachments/830137099042816080/984895322184634448/devcircle_1.png" />
+      ) : (
+        squareValue
+      )}
+    </div>
+  );
+};
 
 export default Square;
